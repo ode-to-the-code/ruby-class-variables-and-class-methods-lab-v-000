a58c543aa3614576d92a1ee2dfe61629e914e577
Jad Abdallah <justcallmethis@gmail.com>
2018-12-05 15:24:15 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,32 @@
+class Song 
+  attr_accessor :name, :artist, :genre 
+  
+  @@count = 0
+  @@genres = []
+  @@artists = []
+  def initialize(name, artist, genre)
+    @@count += 1
+    @name = name 
+    @artist = artist
+    @@artists << artist
+    @genre = genre 
+    @@genres << genre 
+  end 
+  
+  def self.count 
+    @@count 
+  end 
+  
+  def self.genres 
+    @@genres.uniq 
+  end 
+  
+  def self.artists
+    @@artists.uniq
+  end 
+  
+  def self.genre_count 
+    
+  end 
+  
+end 
